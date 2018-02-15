17e348beee34e9a32a662464b5713ecbc80e90ea
Karim Chamaa <karim@udacity.com>
2018-02-14 16:55:46 -0800

    Add files via upload

---
+++ SLAM.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/willowgarage.world " &
+sleep 5
+xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+sleep 2
+xterm -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
+sleep 3
+xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun rviz rviz -d ~/catkin_ws/src/RoboND-SLAMLab/slamconfig.rviz" &
+
