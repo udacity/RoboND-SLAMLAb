4ac8bb2cfd1e5a8ca9dfb70edad965eaa8f9391f
Karim Chamaa <karim@udacity.com>
2018-02-14 16:55:36 -0800

    Delete SLAM.sh

--- SLAM.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/willowgarage.world " &
-sleep 5
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
-sleep 2
-xterm -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
-sleep 3
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun rviz rviz -d ~/catkin_ws/src/slamconfig.rviz" &
-
