e1c4ffd80db39eac0d1da51bfa32ffb049fe99e0
karimchamaa <karim@udacity.com>
2018-06-11 14:29:15 +0000

    Workspace

--- SLAM.sh
+++ SLAM.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/willowgarage.world " &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/willowgarage.world " &
 sleep 5
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
 sleep 2
-xterm -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
+xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
 sleep 3
-xterm  -e " cd ~/catkin_ws; source devel/setup.bash ; rosrun rviz rviz -d ~/catkin_ws/src/RoboND-SLAMLab-Private/slamconfig.rviz" &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun rviz rviz -d $(pwd)/slamconfig.rviz" &
 
